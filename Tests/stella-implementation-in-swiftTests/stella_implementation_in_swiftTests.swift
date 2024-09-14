8210d06f90090080751212f3a27a510e3365da43
NAD777 <nehaev07.07.2003@gmail.com>
2024-09-14 14:25:36 +0300

    Move core tests

--- Tests/stella-implementation-in-swiftTests/stella_implementation_in_swiftTests.swift
+++ Tests/stella-implementation-in-swiftTests/stella_implementation_in_swiftTests.swift
@@ -7,13 +7,13 @@ final class stella_implementation_in_swiftTests: XCTestCase {
     
     func testCoreWellTyped() throws {
         let resourcePath = "\(Bundle.module.resourcePath!)/Resources"
-        let filepaths = filepaths(in: resourcePath + "/well-typed")
+        let filepaths = filepaths(in: resourcePath + "/core/well-typed")
         checkWellTyped(filepaths: filepaths)
     }
 
     func testCoreIllTyped() throws {
         let resourcePath = "\(Bundle.module.resourcePath!)/Resources"
-        let filepaths = filepaths(in: resourcePath + "/ill-typed")
+        let filepaths = filepaths(in: resourcePath + "/core/ill-typed")
         checkIllTyped(filepaths: filepaths)
     }
     
